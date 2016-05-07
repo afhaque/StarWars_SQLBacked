d9acc2063d6bc8ba92b2245db73c05f98569d5e4
Ahmed Haque <afhaque@rice.edu>
2016-05-07 04:48:54 -0400

    Linked up to jawsDB

--- app/config/connection.js
+++ app/config/connection.js
@@ -3,13 +3,27 @@
 // *********************************************************************************
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: "Njmitx123",
-    database: "starwars"
-});
+
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "Njmitx123",
+        database: "starwars"
+    },
+
+    jawsDB: {
+        port: 3306,
+        host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'cm0zdmk2ez4igof5',
+        password: "yuhl98jsef0k7ul4",
+        database: "lghowl35ljxl5vao" 
+    }
+}
+
+var connection = mysql.createConnection(source.jawsDB);
 
 
 connection.connect(function(err) {
